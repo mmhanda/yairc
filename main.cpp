e991b4ddef7e5faf655d1e3ab8db6f42e613d4a1
Anas TABITI <atabiti@e1r4p1.1337.ma>
2023-03-03 11:29:24 +0100

    checking ircserv args' validity

--- main.cpp
+++ main.cpp
@@ -3,18 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mhanda <mhanda@student.42.fr>              +#+  +:+       +#+        */
+/*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 23:48:54 by mhanda            #+#    #+#             */
-/*   Updated: 2023/02/24 23:51:31 by mhanda           ###   ########.fr       */
+/*   Updated: 2023/03/03 11:27:13 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //irc
+#include <cstdlib>
+#include <iostream>
+#include <string>
 
-int main()
+int	main(int ac, char **av)
 {
+	size_t	i;
+	int		port;
+
+	if (ac != 3)
+	{
+		std::cerr << "ERROR: Your executable should be run as follows: \t./ircserv <port> <password>" << std::endl;
+		return (0);
+	}
+	else
+	{
+		std::string port_checking(av[1]);
+		i = 0;
+		while (i < port_checking.size())
+		{
+			if (!(std::isdigit(port_checking[i])))
+			{
+				std::cerr << "ERROR: <port> must be numeric" << std::endl;
+				return (0);
+			}
+			std::cout << port_checking[i] << std::endl;
+			i++;
+		}
+		port = std::atoi(av[1]);
+		std::cout << " port  = " << port << std::endl;
+		if (port < 0 || port > 65353)
+		{
+			std::cerr << "ERROR: <port> must be between 0  and 65353" << std::endl;
+			return (0);
+		}
+		//enter here : start exec
+	}
     
-    
-    return 0;
+	return (0);
 }
