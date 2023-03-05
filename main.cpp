37ea910dcffcd2a4ed2ee36c5c1e24dedb40234c
Anas TABITI <atabiti@e1r4p1.1337.ma>
2023-03-05 11:33:36 +0100

    working on input

--- main.cpp
+++ main.cpp
@@ -6,11 +6,12 @@
 /*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 23:48:54 by mhanda            #+#    #+#             */
-/*   Updated: 2023/03/05 10:11:28 by atabiti          ###   ########.fr       */
+/*   Updated: 2023/03/05 11:33:08 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <vector>
 
 int	checker(int ac, char **av)
 {
@@ -64,27 +65,46 @@ int	checker(int ac, char **av)
 
 int	parse_coommand(void)
 {
+	
 	while (1)
 	{
+		std::vector<std::string> splited_line;
 		std::string input;
-		std::getline(std::cin, input);
-		std::string::iterator iter = input.begin();
-		iter = std::remove_if(input.begin(), input.end(), isspace);
-		input.erase(iter, input.end());
+
+		std::getline(std::cin, input); // read a line
+		size_t  position  = 0;
 		std::cout << "input = " << input << std::endl;
-
-/*
-				Command: USER  
-		Parameters: <username> <hostname> <servername> <realname>
-*/
-		if(input.rfind("USER", 0) < input.size())
+		while ((position = input.find(" ")) <  input.size())
 		{
-			
-			std::cout <<"USER is found" << std::endl;
-			input.erase(0,4);
-		std::cout << "input after erase = " << input << std::endl;
+			splited_line.push_back(input.substr(0, position));
+			input.erase(0, position+1);
+			// std::cout << "input = " << input << std::endl;
+			// std::cout << "position = " << position << std::endl;
+			// std::cout << "splited_line = " << splited_line[0] << std::endl;
 		}
-
+		splited_line.push_back(input.substr(0, position));
+		size_t i = 0;
+		while (i < splited_line.size())
+		{
+			std::cout << "splited_line [] = " << splited_line[i] << std::endl;
+			i++;
+		}
+		
+// 		std::string::iterator iter = input.begin();
+		
+// 		iter = std::remove_if(input.begin(), input.end(), isspace);
+// 		input.erase(iter, input.end());
+// 		std::cout << "input = " << input << std::endl;
+// /*
+// 		Command: USER  
+// 		Parameters: <username> <hostname> <servername> <realname>
+// */
+// 		if (input.rfind("USER", 0) < input.size() || input.rfind("/USER", 0) < input.size() )
+// 		{
+// 			std::cout << "USER is found" << std::endl;
+// 			input.erase(0, 4);
+// 			std::cout << "input after erase = " << input << std::endl;
+// 		}
 	}
 }
 
