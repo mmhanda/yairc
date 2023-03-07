0cd47c6ce24e18ace4748a92be048d1eb51f2bb3
Anas TABITI <atabiti@e1r3p1.1337.ma>
2023-03-07 12:38:16 +0100

    use strtok to parse

--- parsing/input_parsing.cpp
+++ parsing/input_parsing.cpp
@@ -6,7 +6,7 @@
 /*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 09:22:56 by atabiti           #+#    #+#             */
-/*   Updated: 2023/03/07 12:21:20 by atabiti          ###   ########.fr       */
+/*   Updated: 2023/03/07 12:38:07 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,86 +29,91 @@ int	parse_coommand(void)
 										// std::cout << "input = " << input << std::endl << std::endl;
 		if (!input.empty())
 		{
-            /*const_cast <new_type> (expression) why? strtok uses char * and input.c_str() return const char* */
+			/*const_cast <new_type> (expression) why? strtok uses char
+				* and input.c_str() return const char* */
 			char *str = const_cast<char *>(input.c_str());
-			str = strtok(str, " ");
-            if(str != NULL)
-        {
-    	    std::cout << "i am here no segfault "  << std::endl;
-			while (str != NULL)
+    /* 
+            strtok() stores the pointer in static variable where did you last time left off , 
+            so on its 2nd call , when we pass the null , strtok() gets the pointer from the static variable .
+            If you provide the same string name , it again starts from beginning.
+    */
+			str = strtok(str, " ");            
+			if (str != NULL)
 			{
-				std::cout << "str|" << str << std::endl;
-				splited_line.push_back(str);
-				str = strtok(NULL, " ");
-			}
+				while (str != NULL)
+				{
+					// std::cout << "str|" << str << std::endl;
+					splited_line.push_back(str);
+					str = strtok(NULL, " ");
+				}
 
-			i = 0;
-			while (i < splited_line.size())
-			{
-				std::cout << "splited_line [] = " << splited_line[i] << std::endl;
-				i++;
-			}
-			/*
+				i = 0;
+				while (i < splited_line.size())
+				{
+					std::cout << "splited_line [] = " << splited_line[i] << std::endl;
+					i++;
+				}
+				/*
  		Command: PASS 
 	Parameters: <password> (RFC 1459)
     ERRORS : ERR_ALREADYREGISTRED  ERR_NEEDMOREPARAMS
 */
-			if (splited_line[0] == "PASS")
-			{
-				std::cout << "PASS Command" << std::endl;
-				if (splited_line.size() != 2)
+				if (splited_line[0] == "PASS")
 				{
-					std::cerr << "461 " << splited_line[0] << " :Not enough parameters" << std::endl;
+					std::cout << "PASS Command" << std::endl;
+					if (splited_line.size() != 2)
+					{
+						std::cerr << "461 " << splited_line[0] << " :Not enough parameters" << std::endl;
+					}
 				}
-			}
-			/*
-			     Command: NICK
+				/*
+				  Command: NICK
 	NICK <nickname> [<hopcount>] (RFC 1459)
 	NICK <nickname> (RFC 2812)
     ERRORS : ERR_NONICKNAMEGIVEN   ERR_ERRONEUSNICKNAME   ERR_NICKNAMEINUSE        ERR_NICKCOLLISION
 */
 
-			if (splited_line[0] == "NICK")
-			{
-				std::cout << "NICK Command" << std::endl;
-				if (splited_line.size() != 2)
+				if (splited_line[0] == "NICK")
 				{
-					std::cerr << "431 " << splited_line[0] << " :No nickname given" << std::endl;
+					std::cout << "NICK Command" << std::endl;
+					if (splited_line.size() != 2)
+					{
+						std::cerr << "431 " << splited_line[0] << " :No nickname given" << std::endl;
+					}
 				}
-			}
 
-			/*
-		       	Command: USER 
+				/*
+			    	Command: USER 
 	USER <username> <hostname> <servername> <realname> (RFC 1459)
 	USER <user> <mode> <unused> <realname> (RFC 2812)
 */
-			if (splited_line[0] == "USER")
-			{
-				std::cout << "PASS USER" << std::endl;
-				if (splited_line.size() > 5 || splited_line.size() < 5)
+				if (splited_line[0] == "USER")
 				{
-					std::cerr << "USER Not enough parameters. \t Parameters: <username> <hostname> <servername> <realname>" << std::endl;
+					std::cout << "PASS USER" << std::endl;
+					if (splited_line.size() > 5 || splited_line.size() < 5)
+					{
+						std::cerr << "USER Not enough parameters. \t Parameters: <username> <hostname> <servername> <realname>" << std::endl;
+					}
+					std::cout << "USER is found" << std::endl;
 				}
-				std::cout << "USER is found" << std::endl;
-			}
 
-			/*			Command: SERVER
+				/*			Command: SERVER
    Parameters: <servername> <hopcount> <info> 
 */
 
-			if (splited_line[0] == "SERVER")
-			{
-				std::cout << "SERVER command" << std::endl;
-				if (splited_line.size() != 1)
-					std::cerr << "SERVER  parameters error " << std::endl;
-			}
-			if (splited_line[0] == "OPER")
-			{
-				if (splited_line.size() != 2)
+				if (splited_line[0] == "SERVER")
 				{
+					std::cout << "SERVER command" << std::endl;
+					if (splited_line.size() != 1)
+						std::cerr << "SERVER  parameters error " << std::endl;
+				}
+				if (splited_line[0] == "OPER")
+				{
+					if (splited_line.size() != 2)
+					{
+					}
 				}
 			}
 		}
-        }
 	}
 }
