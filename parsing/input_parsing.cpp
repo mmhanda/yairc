892e8a80bdd707e84d993ecf2a55cc59fad7b5b2
Anas TABITI <atabiti@e1r4p1.1337.ma>
2023-03-08 11:10:39 +0100

    parsing PART

--- parsing/input_parsing.cpp
+++ parsing/input_parsing.cpp
@@ -6,7 +6,7 @@
 /*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 09:22:56 by atabiti           #+#    #+#             */
-/*   Updated: 2023/03/08 10:50:47 by atabiti          ###   ########.fr       */
+/*   Updated: 2023/03/08 11:10:21 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ int	parse_coommand(void)
 	size_t	i;
 	char	*str;
 	char	*str1;
-	size_t		x;
-	size_t		h;
+	size_t	x;
+	size_t	h;
 
 	/*
   1235 642 TEXT 0-=+ aKU
@@ -40,14 +40,13 @@ int	parse_coommand(void)
 			/*const_cast <new_type> (expression) why? strtok uses char
 				* and input.c_str() return const char* */
 			str = const_cast<char *>(input.c_str());
-			/* 
-            strtok() stores the pointer in static variable where did you last time left off ,
-	
-            so on its 2nd call , when we pass the null ,
+			/*
+			strtok() stores the pointer in static variable where did you last time left off ,
+			so on its 2nd call , when we pass the null ,
 	strtok() gets the pointer from the static variable .
-            If you provide the same string name ,
+			If you provide the same string name ,
 	it again starts from beginning.
-    */
+	*/
 			str = strtok(str, " ");
 			// std::cout << "after input = " << input << std::endl << std::endl;
 			if (str != NULL)
@@ -64,9 +63,9 @@ int	parse_coommand(void)
 					i++;
 				}
 				/*
- 		Command: PASS 
+		Command: PASS
 	Parameters: <password> (RFC 1459)
-    ERRORS : ERR_ALREADYREGISTRED  ERR_NEEDMOREPARAMS
+	ERRORS : ERR_ALREADYREGISTRED  ERR_NEEDMOREPARAMS
 */
 				if (splited_line[0] == "PASS")
 				{
@@ -80,7 +79,7 @@ int	parse_coommand(void)
 					Command: NICK
 	NICK <nickname> [<hopcount>] (RFC 1459)
 	NICK <nickname> (RFC 2812)
-    ERRORS : ERR_NONICKNAMEGIVEN   ERR_ERRONEUSNICKNAME   ERR_NICKNAMEINUSE        ERR_NICKCOLLISION
+	ERRORS : ERR_NONICKNAMEGIVEN   ERR_ERRONEUSNICKNAME   ERR_NICKNAMEINUSE        ERR_NICKCOLLISION
 */
 				if (splited_line[0] == "NICK")
 				{
@@ -91,7 +90,7 @@ int	parse_coommand(void)
 					}
 				}
 				/*
-						Command: USER 
+						Command: USER
 	USER <username> <hostname> <servername> <realname> (RFC 1459)
 	USER <user> <mode> <unused> <realname> (RFC 2812)
 */
@@ -105,7 +104,7 @@ int	parse_coommand(void)
 					std::cout << "USER is found" << std::endl;
 				}
 				/*			Command: SERVER
-   Parameters: <servername> <hopcount> <info> 
+   Parameters: <servername> <hopcount> <info>
 */
 				// if (splited_line[0] == "SERVER")
 				// {
@@ -128,7 +127,7 @@ Operator privileges.
 				}
 				/*
 			Command: QUIT
-  				Parameters: [<Quit message>]
+				Parameters: [<Quit message>]
 	*/
 				if (splited_line[0] == "QUIT")
 				{
@@ -149,61 +148,99 @@ Operator privileges.
 						// ERROR :Closing link: (atabiti@localhost) [Client exited]
 					}
 				}
-			/*    Command: JOIN Parameters: <channel>{,<channel>} [<key>{,<key>}]
-				  example JOIN #foo,#bar fubar,foobar
+				/*    Command: JOIN Parameters: <channel>{,<channel>} [<key>{,<key>}]
+					example JOIN #foo,#bar fubar,foobar
 			*/
 				if (splited_line[0] == "JOIN")
 				{
-					/*use a map of channel name and a password */
-					std::map<std::string, std::string> channels_map;
-					std::cout << "JOIN COMMAND " << std::endl;
-					x = 0;
-					std::vector<std::string> channels;
-					std::vector<std::string> password;
-					while (splited_line[1].find(",") <= splited_line[1].size())
+					if (splited_line.size() <= 1)
 					{
-						channels.push_back((splited_line[1].substr(0,
-										splited_line[1].find(","))));
-						splited_line[1].erase(0, splited_line[1].find(",") + 1);
+						std::cerr << "461 " << splited_line[0] << " :Not enough parameters" << std::endl;
 					}
-					channels.push_back((splited_line[1].substr(0)));
-
-					
-					while (splited_line[2].find(",") <= splited_line[2].size())
+					else
 					{
-						password.push_back((splited_line[2].substr(0,
-										splited_line[2].find(","))));
-						splited_line[2].erase(0, splited_line[2].find(",") + 1);
-					}
-					password.push_back((splited_line[2].substr(0)));
-					
-					
-					h = 0;
-					while (h < channels.size())
-					{
-						channels_map.insert(std::pair<std::string, std::string>(channels[h] , password[h]));
-						if(channels[h].empty())
+						/*use a map of channel name and a password */
+						std::map<std::string, std::string> channels_map;
+						std::cout << "JOIN COMMAND " << std::endl;
+						x = 0;
+						std::vector<std::string> channels;
+						std::vector<std::string> password;
+						while (splited_line[1].find(",") <= splited_line[1].size())
 						{
-							return 0;
+							channels.push_back((splited_line[1].substr(0,
+																		splited_line[1].find(","))));
+							splited_line[1].erase(0, splited_line[1].find(",")
+									+ 1);
 						}
-						std::cout << "channels [" << h << "] ="<< channels[h] << std::endl;
-						h++;
+						channels.push_back((splited_line[1].substr(0)));
+						while (splited_line[2].find(",") <= splited_line[2].size())
+						{
+							password.push_back((splited_line[2].substr(0,
+																		splited_line[2].find(","))));
+							splited_line[2].erase(0, splited_line[2].find(",")
+									+ 1);
+						}
+						password.push_back((splited_line[2].substr(0)));
+						h = 0;
+						while (h < channels.size())
+						{
+							channels_map.insert(std::pair<std::string,
+															std::string>(channels[h],
+																		password[h]));
+							if (channels[h].empty())
+							{
+								return (0);
+							}
+							std::cout << "channels [" << h << "] =" << channels[h] << std::endl;
+							h++;
+						}
+						h = 0;
+						while (h < password.size())
+						{
+							std::cout << "password [" << h << "] =" << password[h] << std::endl;
+							h++;
+						}
+						/*map */
+						std::map<std::string, std::string>::iterator it;
+						it = channels_map.begin();
+						while (it != channels_map.end())
+						{
+							std::cout << it->first << "::" << it->second << std::endl;
+							++it;
+						}
 					}
-					h = 0;
-					while (h < password.size())
+				}
+				/*
+					Command: PART
+								Parameters: <channel>{,<channel>}
+								example   PART #oz-ops,&group5
+				*/
+				if (splited_line[0] == "PART")
+				{
+					std::cout << "PART COMMAND" << std::endl;
+					if (splited_line.size() <= 1 || splited_line.size() >= 3) /* why 3? to avoid : PART #oz-ops, &dsd   there is a space after ,*/
 					{
-						std::cout << "password [" << h << "] ="<< password[h] << std::endl;
-						h++;
+						std::cerr << "461 " << splited_line[0] << " :Not enough parameters" << std::endl;
 					}
-					/*map */
-					std::map<std::string, std::string>::iterator it;
-					it  = channels_map.begin();
-					while (it  != channels_map.end())
+					else
 					{
-						std::cout << it->first << "::" << it->second<<std::endl;
-						++it;
+						std::vector<std::string> channels;
+						while (splited_line[1].find(",") <= splited_line[1].size())
+						{
+							channels.push_back((splited_line[1].substr(0,
+																		splited_line[1].find(","))));
+							splited_line[1].erase(0, splited_line[1].find(",")
+									+ 1);
+						}
+						channels.push_back((splited_line[1].substr(0)));
+						h = 0;
+						while (h < channels.size())
+						{
+							std::cout << "channels [" << h << "] =" << channels[h] << std::endl;
+							h++;
+						}
+
 					}
-					
 				}
 			}
 		}
