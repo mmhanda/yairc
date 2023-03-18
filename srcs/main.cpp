016ef6dfa77772abf509aee16b2c486c26e07817
Anas Rchid <archid-@stdent.1337.ma>
2023-03-18 04:53:01 +0100

    fixed formatting

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 //   By: archid <archid-@1337.student.ma>           +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2023/03/05 03:13:39 by archid            #+#    #+#             //
-//   Updated: 2023/03/14 23:40:07 by archid           ###   ########.fr       //
+//   Updated: 2023/03/18 04:51:43 by archid           ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -18,48 +18,46 @@
 #define NUM_DIGITS 5
 #define PASSWD_SIZE 32
 
-namespace yairc {
-	short num_port;
-	char *passwd;
+short num_port;
+char *passwd;
 
-	void parse_args(int argc, char *argv[]) {
-		if (argc != 3) {
-			std::cerr << "run as " << argv[0] << " <PORT> <PASSWD>\n";
-			exit(EXIT_FAILURE);
-		}
-
-		for (const char *port = argv[1]; *port != '\0'; ++port) {
-			if (!std::isdigit(*port) || port - argv[1] > NUM_DIGITS) {
-				std::cerr << argv[1] << " is not correct\n";
-				exit(EXIT_FAILURE);
-			}
-		}
-
-		int tmp = std::atoi(argv[1]);
-		if ((int)SHRT_MAX < tmp) {
-			std::cerr << argv[1] << " is invalid\n";
-			exit(EXIT_FAILURE);
-		}
-		num_port = tmp;
-
-		for (const char *passwd = argv[2]; *passwd != '\0'; ++passwd) {
-			if (!std::isprint(*passwd) || passwd - argv[2] > PASSWD_SIZE) {
-				std::cerr << argv[2] << " is invalid\n";
-				exit(EXIT_FAILURE);
-			}
-		}
-		passwd = argv[2];
+void parse_args(int argc, char *argv[]) {
+	if (argc != 3) {
+		std::cerr << "run as " << argv[0] << " <PORT> <PASSWD>\n";
+		exit(EXIT_FAILURE);
 	}
 
-} // namespace yairc
+	for (const char *port = argv[1]; *port != '\0'; ++port) {
+		if (!std::isdigit(*port) || port - argv[1] > NUM_DIGITS) {
+			std::cerr << argv[1] << " is not correct\n";
+			exit(EXIT_FAILURE);
+		}
+	}
 
-yairc::server server(yairc::num_port);
+	int tmp = std::atoi(argv[1]);
+	if ((int)SHRT_MAX < tmp) {
+		std::cerr << argv[1] << " is invalid\n";
+		exit(EXIT_FAILURE);
+	}
+	num_port = tmp;
+
+	for (const char *passwd = argv[2]; *passwd != '\0'; ++passwd) {
+		if (!std::isprint(*passwd) || passwd - argv[2] > PASSWD_SIZE) {
+			std::cerr << argv[2] << " is invalid\n";
+			exit(EXIT_FAILURE);
+		}
+	}
+	passwd = argv[2];
+}
+
+
+server server(num_port);
 
 void handler(int) { server.~server(); }
 
 int main(int argc, char *argv[]) {
 	signal(SIGSTOP, handler);
-	yairc::parse_args(argc, argv);
+	parse_args(argc, argv);
 	try {
 
 		server.run();
