f18f036bc2c6a934391ad5b5980a2f61aeabc15f
Mohammed Handa <mhanda@student.1337.ma>
2023-03-30 13:53:46 +0000

    JUST charachters are allowed in the channel name

--- commands/check_MODE.cpp
+++ commands/check_MODE.cpp
@@ -5,11 +5,14 @@ int check_MODE(std::vector<std::string> &splited_line, user *user_)
 {
     std::string	broad ;
 
-    if (std::find(server_user_names.begin(), server_user_names.end(), splited_line[1]) != server_user_names.end())
+    if (std::find(server_user_names.begin(), server_user_names.end(), splited_line[1]) == server_user_names.end()
+                || user_->chan == nullptr)
+        return 0;
+
         if (user_->chan->how_many_usr() > 1 && splited_line.size() == 3
-            && user_->username() != splited_line[1] && std::find(splited_line[1].begin(),
-                splited_line[1].end(), '#') == splited_line[1].end()) {
-    std::cout << "HERE\n";
+            && user_->username() != splited_line[1]
+            && user_->chan->check_if_s_user_in_by_name(user_->username())) {
+
                 if ((std::find(splited_line[2].begin(), splited_line[2].end(), '+') != splited_line[2].end()
                         && std::find(splited_line[2].begin(), splited_line[2].end(), 'o') != splited_line[2].end())) {
                                     
