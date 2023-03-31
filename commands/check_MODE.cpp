482d46e98cb43fd8f4be0b4b8cb26d90f21e5f64
Mohammed Handa <mhanda@student.1337.ma>
2023-03-31 03:42:51 +0000

    WIP fixing quit

--- commands/check_MODE.cpp
+++ commands/check_MODE.cpp
@@ -66,50 +66,44 @@ int check_MODE(std::vector<std::string> &splited_line, user *user_)
     else if (splited_line.size() <= 4 && std::find(channels_name.begin(), channels_name.end(),
                 splited_line[1]) != channels_name.end() && user_->chan->name() == splited_line[1]) {
 
-            if ((std::find(splited_line[2].begin(), splited_line[2].end(), '+') != splited_line[2].end()
-                    && std::find(splited_line[2].begin(), splited_line[2].end(), 'l') != splited_line[2].end())
-                    && splited_line.size() == 4 && splited_line[2].size() == 2) {
+        if ((std::find(splited_line[2].begin(), splited_line[2].end(), '+') != splited_line[2].end()
+                && std::find(splited_line[2].begin(), splited_line[2].end(), 'l') != splited_line[2].end())
+                && splited_line.size() == 4 && splited_line[2].size() == 2) {
 
-                if (user_->chan->check_if_s_user_in_by_name(user_->username())) {
+            if (user_->chan->check_if_s_user_in_by_name(user_->username())) {
 
-                    if (atoi(splited_line[3].c_str()) >= 1) {
+                if (atoi(splited_line[3].c_str()) >= 1) {
 
-                        user_->chan->set_chan_limit(atoi(splited_line[3].c_str()));
-                        broad = "NOTICE " + user_->chan->name() + " :limit has changed\r\n";
-                        send(user_->client_fd(), broad.c_str(), broad.size(), 0);
-                    }
-                    return (0);
-                }
-                else {
-                    broad = "NOTICE " + user_->chan->name() + " :Only operators allowed\r\n";
+                    user_->chan->set_chan_limit(atoi(splited_line[3].c_str()));
+                    broad = "NOTICE " + user_->chan->name() + " :limit has changed\r\n";
                     send(user_->client_fd(), broad.c_str(), broad.size(), 0);
-                    return 0;
                 }
+                return (0);
             }
-
-            else if ((std::find(splited_line[2].begin(), splited_line[2].end(), '-') != splited_line[2].end()
-                    && std::find(splited_line[2].begin(), splited_line[2].end(), 'l') != splited_line[2].end())
-                    && splited_line.size() == 3 && splited_line[2].size() == 2) {
-
-                if (user_->chan->check_if_s_user_in_by_name(user_->username())) {
-
-                    user_->chan->remove_limit();
-                    broad = "NOTICE " + user_->chan->name() + " :limit has disabled\r\n";
-                    send(user_->client_fd(), broad.c_str(), broad.size(), 0);
-                    return (0);
-                }
-                else {
-                    broad = "NOTICE " + user_->chan->name() + " :Only operators allowed\r\n";
-                    send(user_->client_fd(), broad.c_str(), broad.size(), 0);
-                    return 0;
-                }
-            }
-            else if (splited_line[2] != "+sn")
-            {
-                broad = "NOTICE " + user_->chan->name() + " :invalid operation\r\n";
+            else {
+                broad = "NOTICE " + user_->chan->name() + " :Only operators allowed\r\n";
                 send(user_->client_fd(), broad.c_str(), broad.size(), 0);
                 return 0;
             }
+        }
+
+        else if ((std::find(splited_line[2].begin(), splited_line[2].end(), '-') != splited_line[2].end()
+                && std::find(splited_line[2].begin(), splited_line[2].end(), 'l') != splited_line[2].end())
+                && splited_line.size() == 3 && splited_line[2].size() == 2) {
+
+            if (user_->chan->check_if_s_user_in_by_name(user_->username())) {
+
+                user_->chan->remove_limit();
+                broad = "NOTICE " + user_->chan->name() + " :limit has disabled\r\n";
+                send(user_->client_fd(), broad.c_str(), broad.size(), 0);
+                return (0);
+            }
+            else {
+                broad = "NOTICE " + user_->chan->name() + " :Only operators allowed\r\n";
+                send(user_->client_fd(), broad.c_str(), broad.size(), 0);
+                return 0;
+            }
+        }
     }
 
     return 0;
--- srcs/cmds.cpp
+++ srcs/cmds.cpp
@@ -10,15 +10,49 @@ int check_QUIT(std::string &back_up_input, user *user)
 	str1 = strtok(NULL, ":");
 	if (str1 != NULL)
 	{
+		if (user->chan != nullptr) {
+		std::vector<std::string> msg;
+		msg.push_back("PART");
+		msg.push_back(user->username() );
+		msg.push_back(user->chan->name());
+			check_PART(msg, user);
+			// user->chan->part_user(user);
+		}
 		std::string message = "QUIT ERROR :Closing link: [";
 		message = message + str1 + "]\n";
-		::send(user->client_fd(), message.c_str(), message.length(), 0);
+		// ::send(user->client_fd(), message.c_str(), message.length(), 0);
+		user->NICK_authenticated = false;
+		user->PASS_authenticated = false;
+		user->USER_authenticated = false;
+		server_nick_names.erase(std::remove(server_nick_names.begin(),
+			server_nick_names.end(),user->nickname()),server_nick_names.end());
+		server_user_names.erase(std::remove(server_user_names.begin(),
+			server_user_names.end(),user->username()),server_user_names.end());
+
 		close(user->client_fd());
+
 	}
 	if (str1 == NULL)
 	{
 		std::string message = "QUIT ERROR :Closing link: [User exited]\r\n";
 		::send(user->client_fd(), message.c_str(), message.length(), 0);
+		if (user->chan != nullptr) {
+		std::vector<std::string> msg;
+		msg.push_back("PART");
+		msg.push_back(user->username() );
+		msg.push_back(user->chan->name());
+			check_PART(msg, user);
+			// user->chan->part_user(user);
+		}
+		// ::send(user->client_fd(), message.c_str(), message.length(), 0);
+		user->NICK_authenticated = false;
+		user->PASS_authenticated = false;
+		user->USER_authenticated = false;
+		server_nick_names.erase(std::remove(server_nick_names.begin(),
+			server_nick_names.end(),user->nickname()),server_nick_names.end());
+		server_user_names.erase(std::remove(server_user_names.begin(),
+			server_user_names.end(),user->username()),server_user_names.end());
+
 		close(user->client_fd());
 	}
 
